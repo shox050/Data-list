5e0ef5f5a5fdf5cde0aa56d6ae7a6f6c42db8e32
shox050 <shox050@gmail.com>
2019-09-25 21:49:51 +0300

    Initial commit.

---
+++ Data-list/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Data-list
+//
+//  Created by Vladimir on 25/09/2019.
+//  Copyright © 2019 VladimirYakutin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
